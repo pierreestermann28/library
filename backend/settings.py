850a281e0a46c0481f0a148a2f79e5897f337c08
Pierre Estermann <p.estermann@hotmail.fr>
2020-04-20 19:24:36 +0200

    test

--- backend/settings.py
+++ backend/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, '/build/static')
 ]
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
