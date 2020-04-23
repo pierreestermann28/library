d264593cc0d518a578ba6252a01e2a2181194fb3
Pierre Estermann <p.estermann@hotmail.fr>
2020-04-23 15:01:29 +0200

    Ajout des tests pour le model et le schema author

--- ebooks/tests.py
+++ ebooks/tests.py
@@ -4,45 +4,8 @@ from ebooks.models import Author
 from backend import schema
 import json
 
-class AuthorModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # Set up non-modified objects used by all test methods
-        obj = Author.objects.create(first_name='Big', last_name='Bob', email = 'Bigbob@hotmail.fr')
-        obj.save()
 
-    """Test des champs label"""
-    def test_first_name_label(self):
-        author = Author.objects.get(id=1)
-        field_label = author._meta.get_field('first_name').verbose_name
-        self.assertEquals(field_label, 'first name')
-
-    def test_email_label(self):
-        author=Author.objects.get(id=1)
-        field_label = author._meta.get_field('email').verbose_name
-        self.assertEquals(field_label, 'email')
     
-    def test_last_name_label(self):
-        author=Author.objects.get(id=1)
-        field_label = author._meta.get_field('last_name').verbose_name
-        self.assertEquals(field_label, 'last name')
-
-
-    """Test la longeur max"""
-    def test_first_name_max_length(self):
-        author = Author.objects.get(id=1)
-        max_length = author._meta.get_field('first_name').max_length
-        self.assertEquals(max_length, 30)
-
-    def test_last_name_max_length(self):
-        author = Author.objects.get(id=1)
-        max_length = author._meta.get_field('last_name').max_length
-        self.assertEquals(max_length, 30)
-
-    def test_email_max_length(self):
-        author = Author.objects.get(id=1)
-        max_length = author._meta.get_field('email').max_length
-        self.assertEquals(max_length, 30)
 
         
 class AuthorSchemaTestCase(GraphQLTestCase):
@@ -123,7 +86,7 @@ class AuthorSchemaTestCase(GraphQLTestCase):
     
 
     
-    def test_update_author_with_wrong_email(self):
+    def test_update_author(self):
         response = self.query(
             '''
             mutation updateAuthor($id: Int!, $email: String, $firstName: String,$lastName: String) {
@@ -138,7 +101,7 @@ class AuthorSchemaTestCase(GraphQLTestCase):
             }
             ''',
             op_name='updateAuthor',
-            variables={'id': 2, 'firstName': 'foo2', 'lastName': 'baraka', 'email': 'barakafoo2@hotmail.fr'}
+            variables={'id': 1, 'firstName': 'foo2', 'lastName': 'baraka', 'email': 'barakafoo2@hotmail.fr'}
         )
         content = json.loads(response.content)
         indice = content['data']['updateAuthor']['author']['id']
@@ -148,7 +111,7 @@ class AuthorSchemaTestCase(GraphQLTestCase):
         
         # This validates the status code and if you get errors
         self.assertResponseNoErrors(response) 
-        self.assertEqual('2', indice)
+        self.assertEqual('1', indice)
         self.assertEqual('foo2', new_first_name)
         self.assertEqual('baraka', new_last_name)
         self.assertEqual('barakafoo2@hotmail.fr', new_email)
@@ -164,9 +127,42 @@ class AuthorSchemaTestCase(GraphQLTestCase):
             }
             ''',
             op_name='author',
-            variables={'id':2}
+            variables={'id':1}
         )
 
         content = json.loads(response.content)
         # This validates the status code and if you get errors
         self.assertResponseNoErrors(response)  
+
+    """Test des champs label"""
+    def test_first_name_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEquals(field_label, 'first name')
+
+    def test_email_label(self):
+        author=Author.objects.get(id=1)
+        field_label = author._meta.get_field('email').verbose_name
+        self.assertEquals(field_label, 'email')
+    
+    def test_last_name_label(self):
+        author=Author.objects.get(id=1)
+        field_label = author._meta.get_field('last_name').verbose_name
+        self.assertEquals(field_label, 'last name')
+
+
+    """Test la longeur max"""
+    def test_first_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEquals(max_length, 30)
+
+    def test_last_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('last_name').max_length
+        self.assertEquals(max_length, 30)
+
+    def test_email_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('email').max_length
+        self.assertEquals(max_length, 30)
