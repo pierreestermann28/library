bcb9e5a6cfb9f4baf31c6cb5f0f0148a4317ddef
Pierre Estermann <p.estermann@hotmail.fr>
2020-04-30 17:58:06 +0200

    understanding authentification

--- backend/settings.py
+++ backend/settings.py
@@ -160,6 +160,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [BASE_DIR, 'build', 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
